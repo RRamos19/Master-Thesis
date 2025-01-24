57fd3d90263f1a8e0fd06d66c9799ddaf0d139cf
RRamos19 <ricardo.alexandre.alves.ramos@gmail.com>
2025-01-24 01:25:25 +0000

    Great progress in chapters 1, 2 and 3.

--- build.bat
+++ build.bat
@@ -2,22 +2,25 @@
 CD iselthesis
 
 SET output=template.pdf
+SET final_output=RDT_TFM35_Ricardo_Ramos_46638.pdf
 SET log_file=template.log
 
 SET /p resposta="Atualizar a bibliografia ? (s/n): "
 
 SET or_=false
-IF EXIST %output% SET or_=true
+IF EXIST %final_output% SET or_=true
 IF EXIST %log_file% SET or_=true
 
 IF "%or_%"=="true" (
 	IF /I "%resposta%"=="s" (
 		make clean
 	) else (
-		IF EXIST "%output%" DEL "%output%"
+		IF EXIST "%final_output%" DEL "%final_output%"
 	)
 )
 
 make pdf
 
-start "" "%output%"
+IF EXIST "%output%" REN "%output%" "%final_output%"
+
+start "" "%final_output%"
