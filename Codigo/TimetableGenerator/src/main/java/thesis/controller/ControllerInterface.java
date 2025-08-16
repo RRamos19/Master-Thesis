d641dd32db968c16ee7841b8f81de10166ad83b8
RRamos19 <ricardo.alexandre.alves.ramos@gmail.com>
2025-08-16 03:10:27 +0100

    Advancement in the timetable presentation. Its still not possible to see them.

--- Codigo/TimetableGenerator/src/main/java/thesis/controller/ControllerInterface.java
+++ Codigo/TimetableGenerator/src/main/java/thesis/controller/ControllerInterface.java
@@ -6,15 +6,19 @@ import thesis.model.domain.Timetable;
 import thesis.view.ViewInterface;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Set;
 
 public interface ControllerInterface {
     void setModel(ModelInterface<DataRepository> model);
     void setView(ViewInterface view);
     Timetable generateTimetable(DataRepository data, double initialTemperature, double minTemperature, double coolingRate, int k);
+    DataRepository getDataRepository(String programName);
+    Set<String> getStoredPrograms();
     void importITCData(File file);
-    void exportSolutionsToITC(DataRepository data);
-    void exportDomainToITC(DataRepository data);
-    void exportToCSV(DataRepository data);
-    void exportToPDF(DataRepository data);
-    void exportToPNG(DataRepository data);
+    void exportSolutionsToITC(String programName) throws IOException;
+    void exportDataToITC(String programName) throws IOException;
+    void exportToCSV(String programName) throws IOException;
+    void exportToPDF(String programName) throws IOException;
+    void exportToPNG(String programName) throws IOException;
 }
